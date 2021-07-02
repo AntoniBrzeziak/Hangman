039a3a2d3a42811b98054078f5f8a43ed528f4be
JB <juliusz.brzeziak@student.uj.edu.pl>
2021-07-02 19:59:26 +0200

    Round constructor

--- Hangman/Round/Round.cs
+++ Hangman/Round/Round.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
 
 namespace Hangman.Round
 {
     class Round
     {
+
+        public List<string[]> Pairs = new List<string[]>();
 
         public void PrintStatus()
         {
@@ -26,6 +29,42 @@ namespace Hangman.Round
         {
 
         }
+
+        public Round()
+        {
+
+            string fileName = @"countries_and_capitals.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(fileName))
+                {
+
+                    string s = "";
+                    string[] tab;
+
+                    s = sr.ReadLine();
+
+                    while ((s = sr.ReadLine()) != null)
+                    {
+
+                        tab = s.Split(" | ");
+                        Pairs.Add(tab);
+
+                    }
+                }
+            }
+
+            catch (Exception Ex)
+            {
+                Console.WriteLine(Ex.ToString());
+            }
+
+        }
+
+
+
+
 
     }
 
